ca641b72eff388e0976262461315d31ad46c4f7c
Barbara <huem.barbara@gmail.com>
2023-08-28 22:06:42 +0300

    added intro Text, CV, languages

--- pages/index.js
+++ pages/index.js
@@ -14,9 +14,25 @@ export default function Home() {
         <h1 className={styles.title}>
           I am Barbara Huemer
         </h1>
-        <div className={styles.circle}></div>
-        <div className={styles.triangle}></div>
-        <img src="/img/3D_me.png"/>
+        <div className={styles.blur}>
+        <img src="/img/3D_me.png" height={200}/>
+          <div className={styles.circle}></div>
+          <div className={styles.triangle}></div>
+        </div>
+        <div className={styles.circleGroup}> 
+          <div className={styles.circleOne}></div>
+          <div className={styles.circleTwo}></div>
+          <div className={styles.circleThree}></div>
+        </div>
+
+        <div className={styles.intro}>
+          <p className={styles.introText}>This is my digital space where I showcase my skills, 
+            experiences, and projects. Whether you stumbled upon this site 
+            accidentally or are here intentionally, I'm glad you stopped by!</p>
+          <button>
+            <a className={styles.button} href="CV_Huemer-Barbara.pdf">CV</a>
+          </button>
+        </div>
 
         <div className={styles.grid}>
 
@@ -40,75 +56,12 @@ export default function Home() {
             <p>react</p>
           </a>
 
-          <a className={styles.card}>
-            <img src="/img/logos/next.png"/>
-            <p>next</p>
-          </a>
-
         </div>
       </main>
 
       <footer>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel" className={styles.logo} />
-        </a>
+        <p>Barbara Huemer</p>
       </footer>
-
-      <style jsx>{`
-        main {
-          padding: 5rem 0;
-          flex: 1;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-        footer {
-          width: 100%;
-          height: 100px;
-          border-top: 1px solid #eaeaea;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-        footer img {
-          margin-left: 0.5rem;
-        }
-        footer a {
-          display: flex;
-          justify-content: center;
-          align-items: center;
-          text-decoration: none;
-          color: inherit;
-        }
-        code {
-          background: #fafafa;
-          border-radius: 5px;
-          padding: 0.75rem;
-          font-size: 1.1rem;
-          font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
-            DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
-        }
-      `}</style>
-
-      <style jsx global>{`
-        html,
-        body {
-          padding: 0;
-          margin: 0;
-          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
-            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
-            sans-serif;
-        }
-        * {
-          box-sizing: border-box;
-        }
-      `}</style>
     </div>
   )
 }
